9c0d3a122c032729c4af590b36684a75cd4aafc8
yots2016 <newstageja@gmail.com>
2017-02-22 16:50:54 +0200

    Week 2 Lection 6

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+  static int staticDemo;
+
+  public static void main (String[] args) {
+    int anArray[] = new int[10];
+    String testString = "Hello World!";
+    anArray[0] = 100;
+    anArray[1] = 200;
+
+    System.out.println(anArray.length);
+  }  
+}
